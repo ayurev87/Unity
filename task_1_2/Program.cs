40f19103c6a066dc6ee052f8cd530d0b622cc6ce
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-23 08:39:25 +0300

    дз1

---
+++ task_1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[100];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+   while (index < length)
+   {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+   }
+}
+FillArray(array);
+
+foreach (int i in array)
+{
+    Console.WriteLine(i);
+}
